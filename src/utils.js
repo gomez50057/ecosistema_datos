63b60e8454d21afd8a8d4386a8b3284f85ee02ea
gomez50057 <gomez.50057@gmail.com>
2024-06-10 12:11:29 -0600

    cards

--- src/utils.js
+++ src/utils.js
@@ -1,45 +1,45 @@
 // src/utils.js
 export const cardData = [
   {
-    img: 'https://github.com/kunaal438/card-hover/blob/master/img.jpg?raw=true',
-    link: 'https://www.example1.com',
-    title: 'Card 01',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aperiam fugiat eos odit numquam vitae facere dolore id libero sit!'
+    img: 'img/cards/1.jpg',
+    link: 'https://lookerstudio.google.com/reporting/b3852579-e11c-42a5-b3ce-e0cea4cd03cc/page/TpTjD/edit',
+    title: 'Centro Deportivo Hidalguense y de Alto Rendimiento',
+    description: 'Reporte estadístico de la Consulta sobre tu experiencia en el Centro Deportivo Hidalguense y de Alto Rendimiento.'
   },
   {
-    img: 'https://github.com/kunaal438/card-hover/blob/master/img.jpg?raw=true',
-    link: 'https://www.example2.com',
-    title: 'Card 02',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aperiam fugiat eos odit numquam vitae facere dolore id libero sit!'
+    img: 'img/cards/2.jpg',
+    link: 'https://lookerstudio.google.com/reporting/0e561a04-0f72-41cf-afe3-97f95a0bfba6/page/TpTjD',
+    title: 'Calidad de internet y mejora regulatoria',
+    description: 'Reporte estadístico de la Consulta con la ciudadanía en materia de mejora regulatoria y calidad de internet.'
   },
   {
-    img: 'https://github.com/kunaal438/card-hover/blob/master/img.jpg?raw=true',
-    link: 'https://www.example3.com',
-    title: 'Card 03',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aperiam fugiat eos odit numquam vitae facere dolore id libero sit!'
+    img: 'img/cards/3.jpg',
+    link: 'https://lookerstudio.google.com/reporting/9745562c-cc2c-4aaa-bd4f-c4b92c5e63e4/page/TpTjD',
+    title: 'Consulta Programas Municipales de Desarrollo Urbano',
+    description: 'Reporte general de los levantamientos estadísticos para los programas municipales de desarrollo urbano'
   },
   {
-    img: 'https://github.com/kunaal438/card-hover/blob/master/img.jpg?raw=true',
-    link: 'https://www.example4.com',
-    title: 'Card 04',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aperiam fugiat eos odit numquam vitae facere dolore id libero sit!'
+    img: 'img/cards/4.jpg',
+    link: 'https://lookerstudio.google.com/reporting/76a68089-17eb-4357-b66f-158089ea635f/page/TpTjD',
+    title: 'Consulta General de Programas de Desarrollo Regional (Rutas)',
+    description: 'Reporte de la Consulta General de Programas de Desarrollo Regional.'
   },
   {
-    img: 'https://github.com/kunaal438/card-hover/blob/master/img.jpg?raw=true',
-    link: 'https://www.example5.com',
-    title: 'Card 05',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aperiam fugiat eos odit numquam vitae facere dolore id libero sit!'
+    img: 'img/cards/5.jpg',
+    link: 'https://lookerstudio.google.com/reporting/f08bda71-6318-4167-b266-0764742c3946/page/yz2WD',
+    title: 'Polígono Movilidad',
+    description: 'Polígono de movilidad sustentable'
   },
   {
-    img: 'https://github.com/kunaal438/card-hover/blob/master/img.jpg?raw=true',
-    link: 'https://www.example6.com',
-    title: 'Card 06',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aperiam fugiat eos odit numquam vitae facere dolore id libero sit!'
+    img: 'img/cards/trabakando.jpg',
+    link: 'https://lookerstudio.google.com/reporting/3a03a833-6915-43a7-bece-9c96618078dd/page/TpTjD',
+    title: 'Foros Regionales Mujeres',
+    description: 'Foros regionales. Elaboración de la Ruta de Actuación Interinstitucional para la Intervención con hombres generadores de violencia contra las mujeres y las niñas 2023-2024.'
   },
 ];
 
 export const fullCardData = {
-  img: 'https://github.com/kunaal438/card-hover/blob/master/img.jpg?raw=true',
+  img: 'img/cards/trabakando.jpg',
   link: 'https://lookerstudio.google.com/reporting/0c5c7e57-84af-4fb0-8faa-5e0b5b96b338/page/yz2WD',
   title: 'Informe general de propuestas y solicitudes ciudadanas',
   description: 'This is a full width card with a height of 450px.',
